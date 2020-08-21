f6306df70acf1e003d5e62503707b93bd9a2d474
Miroslav <miroslav.miki.stojakovic@gmail.com>
2020-08-21 14:53:33 +0200

    Added ValuesController

---
+++ Reactivities/API/Controllers/ValuesController.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers
+{
+ [Route("api/[controller]")]
+ [ApiController]
+ public class ValuesController : ControllerBase
+ {
+  private readonly DataContext _context;
+
+  public ValuesController(DataContext context)
+  {
+   _context = context;
+
+  }
+  [HttpGet]
+  public async Task<ActionResult<IEnumerable<Value>>> Get()
+  {
+   var result = await _context.Values.ToListAsync();
+   return Ok(result);
+  }
+
+  [HttpGet("{id}")]
+  public async Task<ActionResult<Value>> Get(int id)
+  {
+   var result = await _context.Values.FindAsync(id);
+   return Ok(result);
+  }
+
+ }
+}
