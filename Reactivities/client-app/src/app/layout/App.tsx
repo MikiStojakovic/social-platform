1ad895cf208f7245a2ecdccf243cd0d52258c7f9
Miroslav <miroslav.miki.stojakovic@gmail.com>
2020-09-07 14:38:22 +0200

    Added selectedActivity state and selectActivityHandler

--- Reactivities/client-app/src/app/layout/App.tsx
+++ Reactivities/client-app/src/app/layout/App.tsx
@@ -8,6 +8,13 @@ import { act } from 'react-dom/test-utils';
 
 const App = () => {
   const [activities, setActivities] = useState<IActivity[]>([]);
+  const [selectedActivity, setSelectedActivity] = useState<IActivity | null>(
+    null
+  );
+
+  const handleSelectActivity = (id: string) => {
+    setSelectedActivity(activities.filter((a) => a.id === id)[0]);
+  };
 
   useEffect(() => {
     axios
@@ -21,7 +28,11 @@ const App = () => {
     <Fragment>
       <NavBar></NavBar>
       <Container style={{ marginTop: '7em' }}>
-        <ActivityDashboard activities={activities}></ActivityDashboard>
+        <ActivityDashboard
+          activities={activities}
+          selectActivity={handleSelectActivity}
+          selectedActivity={selectedActivity}
+        ></ActivityDashboard>
       </Container>
     </Fragment>
   );
