72a9d26dc445ba66424acf141c9b68f8bf4e8c4e
Miroslav <miroslav.miki.stojakovic@gmail.com>
2020-09-11 16:28:21 +0200

    Removed handleEditActivity from App component

--- Reactivities/client-app/src/app/layout/App.tsx
+++ Reactivities/client-app/src/app/layout/App.tsx
@@ -25,20 +25,6 @@ const App = () => {
   const [submitting, setSubmitting] = useState(false);
   const [target, setTarget] = useState('');
 
-  const handleEditActivity = (activity: IActivity) => {
-    setSubmitting(true);
-    agent.Activities.update(activity)
-      .then(() => {
-        setActivities([
-          ...activities.filter((a) => a.id !== activity.id),
-          activity,
-        ]);
-        setSelectedActivity(activity);
-        setEditMode(false);
-      })
-      .then(() => setSubmitting(false));
-  };
-
   const handleDeleteActivity = (
     event: SyntheticEvent<HTMLButtonElement>,
     id: string
