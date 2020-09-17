5d8377b97514667c4db3f1289e64c381e7ece7ed
Miroslav <miroslav.miki.stojakovic@gmail.com>
2020-09-17 12:08:58 +0200

    Added CommandValidators for Create and Edit Activity Commands

--- Reactivities/Application/Activities/Create.cs
+++ Reactivities/Application/Activities/Create.cs
@@ -2,6 +2,7 @@ using System;
 using System.Threading;
 using System.Threading.Tasks;
 using Domain;
+using FluentValidation;
 using MediatR;
 using Persistence;
 
@@ -26,6 +27,19 @@ namespace Application.Activities
    public Handler(DataContext context)
    {
     _context = context;
+   }
+
+   public class CommandValidator : AbstractValidator<Command>
+   {
+    public CommandValidator()
+    {
+     RuleFor(x => x.Title).NotEmpty();
+     RuleFor(x => x.Description).NotEmpty();
+     RuleFor(x => x.Category).NotEmpty();
+     RuleFor(x => x.Date).NotEmpty();
+     RuleFor(x => x.City).NotEmpty();
+     RuleFor(x => x.Venue).NotEmpty();
+    }
    }
 
    public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
