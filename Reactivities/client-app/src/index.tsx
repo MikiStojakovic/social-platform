4935a87ffe2fdf30462aae5ea9f5544fdf3080db
Miroslav <miroslav.miki.stojakovic@gmail.com>
2020-09-18 12:03:40 +0200

    Raise toast popup on server errors

--- Reactivities/client-app/src/index.tsx
+++ Reactivities/client-app/src/index.tsx
@@ -4,6 +4,7 @@ import './app/layout/styles.css';
 import App from './app/layout/App';
 import * as serviceWorker from './serviceWorker';
 import { Router } from 'react-router-dom';
+import 'react-toastify/dist/ReactToastify.min.css';
 import { createBrowserHistory } from 'history';
 import ScrollToTop from './app/layout/ScrollToTop';
 
