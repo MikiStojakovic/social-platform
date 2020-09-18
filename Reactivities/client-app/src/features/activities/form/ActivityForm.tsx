cf737afd83e9cf8f0732b93adac32b3e9976b8a7
Miroslav <miroslav.miki.stojakovic@gmail.com>
2020-09-18 15:00:30 +0200

    Added TextInput component

--- Reactivities/client-app/src/features/activities/form/ActivityForm.tsx
+++ Reactivities/client-app/src/features/activities/form/ActivityForm.tsx
@@ -6,6 +6,7 @@ import ActivityStore from '../../../app/stores/activityStore';
 import { observer } from 'mobx-react-lite';
 import { RouteComponentProps } from 'react-router-dom';
 import { Form as FinalForm, Field } from 'react-final-form';
+import TextInput from '../../../app/common/form/TextInput';
 
 interface IDetailParams {
   id: string;
@@ -91,7 +92,7 @@ const ActivityForm: React.FC<RouteComponentProps<IDetailParams>> = ({
                   placeholder="Title"
                   name="title"
                   value={activity.title}
-                  component="input"
+                  component={TextInput}
                 />
                 <Form.TextArea
                   onChange={handleInputChange}
