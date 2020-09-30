63635c7bcd31758b1b3c611a28b4381bc0a81ac0
Miroslav <miroslav.miki.stojakovic@gmail.com>
2020-09-30 11:48:29 +0200

    Added logic to show submission errors

--- Reactivities/client-app/src/features/user/loginForm.tsx
+++ Reactivities/client-app/src/features/user/loginForm.tsx
@@ -1,6 +1,7 @@
+import { FORM_ERROR } from 'final-form';
 import React, { useContext } from 'react';
 import { Form as FinalForm, Field } from 'react-final-form';
-import { Button, Form } from 'semantic-ui-react';
+import { Button, Form, Label } from 'semantic-ui-react';
 import TextInput from '../../app/common/form/TextInput';
 import { IUserFormValues } from '../../app/models/user';
 import { RootStoreContext } from '../../app/stores/rootStore';
@@ -11,8 +12,12 @@ export const LoginForm = () => {
 
   return (
     <FinalForm
-      onSubmit={(values: IUserFormValues) => login(values)}
-      render={({ handleSubmit }) => (
+      onSubmit={(values: IUserFormValues) =>
+        login(values).catch((error) => ({
+          [FORM_ERROR]: error,
+        }))
+      }
+      render={({ handleSubmit, submitting, form, submitError }) => (
         <Form onSubmit={handleSubmit}>
           <Field name="email" component={TextInput} placeholder="Email" />
           <Field
@@ -21,7 +26,12 @@ export const LoginForm = () => {
             placeholder="Password"
             type="password"
           />
-          <Button positive content="Login" />
+          {submitError && (
+            <Label color="red" basic content={submitError.statusText} />
+          )}
+          <br />
+          <Button loading={submitting} positive content="Login" />
+          <pre>{JSON.stringify(form.getState(), null, 2)}</pre>
         </Form>
       )}
     />
