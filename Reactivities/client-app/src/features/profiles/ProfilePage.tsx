f8d1be281356e83f02394bf5af5e010e0ed84419
Miroslav <miroslav.miki.stojakovic@gmail.com>
2020-10-15 16:47:46 +0200

    Added ProfilePage component; Added profile route

---
+++ Reactivities/client-app/src/features/profiles/ProfilePage.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Grid } from 'semantic-ui-react'
+import ProfileHeader from './ProfileHeader'
+
+export const ProfilePage = () => {
+ return (
+  <Grid>
+   <Grid.Column width={16}>
+    <ProfileHeader/>
+   </Grid.Column>
+   </Grid>
+ )
+}
