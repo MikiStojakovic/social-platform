32551cce0c34438203ea67d7f28895092483ed42
Miroslav <miroslav.miki.stojakovic@gmail.com>
2020-10-23 17:29:13 +0200

    Added uploadPhoto action to profileStore

--- Reactivities/client-app/src/app/api/agent.ts
+++ Reactivities/client-app/src/app/api/agent.ts
@@ -2,7 +2,7 @@ import axios, { AxiosResponse } from 'axios';
 import { toast } from 'react-toastify';
 import { history } from '../..';
 import { IActivity } from '../models/activity';
-import { IProfile } from '../models/profile';
+import { IPhoto, IProfile } from '../models/profile';
 import { IUser, IUserFormValues } from '../models/user';
 
 axios.defaults.baseURL = 'http://localhost:5000/api';
@@ -59,6 +59,13 @@ const requests = {
     axios.put(url, body).then(sleep(1000)).then(responseBody),
   delete: (url: string) =>
     axios.delete(url).then(sleep(1000)).then(responseBody),
+    postForm: (url: string, file: Blob) => {
+      let formData = new FormData();
+      formData.append('File', file);
+      return axios.post(url, formData, {
+        headers: {'Content-type': 'multipart/form-data'}
+      }).then(responseBody)
+    }
 };
 
 const Activities = {
@@ -81,7 +88,8 @@ const User = {
 };
 
 const Profiles = {
-  get: (username: string): Promise<IProfile> => requests.get(`/profiles/${username}`)
+  get: (username: string): Promise<IProfile> => requests.get(`/profiles/${username}`),
+  uploadPhoto: (photo: Blob): Promise<IPhoto> => requests.postForm(`/photos`, photo)
 }
 
 export default {
--- Reactivities/client-app/src/app/stores/profileStore.ts
+++ Reactivities/client-app/src/app/stores/profileStore.ts
@@ -1,4 +1,7 @@
+import { timeStamp } from "console";
+import { tr } from "date-fns/locale";
 import { action, observable, runInAction, computed } from "mobx";
+import { toast } from "react-toastify";
 import agent from "../api/agent";
 import { IProfile } from "../models/profile";
 import { RootStore } from "./rootStore";
@@ -11,6 +14,7 @@ export default class ProfileStore {
 
  @observable profile: IProfile | null = null;
  @observable loadingProfile = true;
+ @observable uploadingPhoto = false;
 
  @computed get isCurrentUser() {
   if (this.rootStore.userStore.user && this.profile) {
@@ -33,6 +37,29 @@ export default class ProfileStore {
     this.loadingProfile = false;
    })
    console.log(error);
+  }
+ }
+
+ @action uploadPhoto = async (file: Blob) => {
+  this.uploadingPhoto = true;
+  try {
+   const photo = await agent.Profiles.uploadPhoto(file);
+   runInAction(() => {
+    if (this.profile) {
+     this.profile.photos.push(photo);
+     if (photo.isMain && this.rootStore.userStore.user) {
+      this.rootStore.userStore.user.image = photo.url;
+      this.profile.image = photo.url;
+     }
+    }
+    this.uploadingPhoto = false;
+   })
+  } catch (error) {
+   console.log(error);
+   toast.error('Problem uploading photo');
+   runInAction(() => {
+    this.uploadingPhoto = false;
+   })
   }
  }
 }
