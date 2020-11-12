1ffd915a0937ac1e41ba6f620c77a8e659ca3046
Miroslav <miroslav.miki.stojakovic@gmail.com>
2020-11-12 13:10:47 +0100

    Added follow and unfollow actions to profileStore

--- Reactivities/client-app/src/app/stores/profileStore.ts
+++ Reactivities/client-app/src/app/stores/profileStore.ts
@@ -1,3 +1,4 @@
+import { tr } from "date-fns/locale";
 import { action, observable, runInAction, computed } from "mobx";
 import { toast } from "react-toastify";
 import agent from "../api/agent";
@@ -96,4 +97,39 @@ export default class ProfileStore {
    })
   }
  }
+
+ @action follow = async (username: string) => {
+  this.loading = true;
+  try {
+   await agent.Profiles.follow(username);
+   runInAction(() => {
+    this.profile!.following = true;
+    this.profile!.followersCount++;
+    this.loading = false;
+   })
+  } catch (error) {
+   toast.error('Problem following user');
+   runInAction(() => {
+    this.loading = false;
+   })
+  }
+ }
+
+ @action unfollow = async (username: string) => {
+  this.loading = true;
+  try {
+   await agent.Profiles.unfollow(username);
+   runInAction(() => {
+    this.profile!.following = false;
+    this.profile!.followersCount--;
+    this.loading = false;
+   })
+  } catch (error) {
+   toast.error('Problem unfollowing user');
+   runInAction(() => {
+    this.loading = false;
+   })
+  }
+ }
 }
+
