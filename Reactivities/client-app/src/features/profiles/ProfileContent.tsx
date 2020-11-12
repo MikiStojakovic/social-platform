524880bfc88d55ecd85815c2505a8cde36cf9b12
Miroslav <miroslav.miki.stojakovic@gmail.com>
2020-11-12 16:35:19 +0100

    Added ProfileCards and ProfileFollowing components

--- Reactivities/client-app/src/features/profiles/ProfileContent.tsx
+++ Reactivities/client-app/src/features/profiles/ProfileContent.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import {Tab} from 'semantic-ui-react';
+import ProfileFollowings from './ProfileFollowings';
 import ProfilePhotos from './ProfilePhotos';
 
 const panes = [
  {menuItem: 'About', render: () => <Tab.Pane>About content</Tab.Pane>},
  {menuItem: 'Photos', render: () => <ProfilePhotos/>},
  {menuItem: 'Activities', render: () => <Tab.Pane>Activities content</Tab.Pane>},
- {menuItem: 'Followers', render: () => <Tab.Pane>Followers content</Tab.Pane>},
- {menuItem: 'Following', render: () => <Tab.Pane>Following content</Tab.Pane>}
+ {menuItem: 'Followers', render: () => <ProfileFollowings/>},
+ {menuItem: 'Following', render: () => <ProfileFollowings/>}
 ]
 
 export const ProfileContent = () => {
