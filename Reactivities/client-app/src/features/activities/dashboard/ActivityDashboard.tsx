d0e4599e0cdf02d9caa6b4f3e18e283d90a98cf8
Miroslav <miroslav.miki.stojakovic@gmail.com>
2020-11-13 13:04:56 +0100

    Implemeted infinite scroll

--- Reactivities/client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ Reactivities/client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,9 +1,10 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { Button, Grid } from 'semantic-ui-react';
+import { Button, Grid, Loader } from 'semantic-ui-react';
 import ActivityList from './ActivityList';
 import { observer } from 'mobx-react-lite';
 import { LoadingComponent } from '../../../app/layout/LoadingComponent';
 import { RootStoreContext } from '../../../app/stores/rootStore';
+import InfiniteScroll from 'react-infinite-scroller';
 
 const ActivityDashboard: React.FC = () => {
   const rootStore = useContext(RootStoreContext);
@@ -26,18 +27,19 @@ const ActivityDashboard: React.FC = () => {
   return (
     <Grid>
       <Grid.Column width={10}>
-        <ActivityList />
-        <Button
-          floated='right'
-          content='More...'
-          positive
-          disabled={totalPages === page + 1}
-          onClick={hanldeGetNext}
-          loading={loadingNext}
-        />
+        <InfiniteScroll 
+          pageStart={0} 
+          loadMore={hanldeGetNext}
+          hasMore={!loadingNext && page + 1 < totalPages}
+          initialLoad={false}>
+          <ActivityList />
+        </InfiniteScroll>        
       </Grid.Column>
       <Grid.Column width={6}>
         <h2>Activity filters</h2>
+      </Grid.Column>
+      <Grid.Column width={10}>
+        <Loader active={loadingNext}/>
       </Grid.Column>
     </Grid>
   );
