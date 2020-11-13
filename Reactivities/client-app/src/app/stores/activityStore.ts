6bea32707c340e22368235c00ac01341202c9572
Miroslav <miroslav.miki.stojakovic@gmail.com>
2020-11-13 11:41:50 +0100

    Added observables and computed properties to enable paging

--- Reactivities/client-app/src/app/stores/activityStore.ts
+++ Reactivities/client-app/src/app/stores/activityStore.ts
@@ -7,6 +7,9 @@ import { toast } from 'react-toastify';
 import { RootStore } from './rootStore';
 import { createAttendee, setActivityProps } from '../common/util/util';
 import { HubConnection, HubConnectionBuilder, LogLevel } from '@microsoft/signalr';
+import { act } from 'react-dom/test-utils';
+
+const LIMIT = 2;
 
 export default class ActivityStore {
   rootStore: RootStore;
@@ -22,6 +25,16 @@ export default class ActivityStore {
   @observable target = '';
   @observable loading = false;
   @observable.ref hubConnection: HubConnection | null = null;
+  @observable activityCount = 0;
+  @observable page = 0;
+
+  @computed get totalPages() {
+    return Math.ceil(this.activityCount / LIMIT);
+  }
+
+  @action setPage = (page: number) => {
+    this.page = page;
+  }
 
   @action createHubConnection = (activitId: string) => {
     this.hubConnection = new HubConnectionBuilder().withUrl('http://localhost:5000/chat', {
@@ -94,12 +107,14 @@ export default class ActivityStore {
   @action loadActivities = async () => {
     this.loadingInitial = true;
     try {
-      const activities = await agent.Activities.list();
+      const activitiesEnvelope = await agent.Activities.list(LIMIT, this.page);
+      const {activities, activityCount} = activitiesEnvelope;
       runInAction('loading activities', () => {
         activities.forEach((activity) => {
           setActivityProps(activity, this.rootStore.userStore.user!);
           this.activityRegistry.set(activity.id, activity);
         });
+        this.activityCount = activityCount;
         this.loadingInitial = false;
       });
     } catch (error) {
