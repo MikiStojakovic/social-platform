be3035554e4e090a22ae2579f21e462478047e05
Miroslav <miroslav.miki.stojakovic@gmail.com>
2020-11-25 12:47:18 +0100

    added loadUserActivities action

--- Reactivities/client-app/src/app/stores/profileStore.ts
+++ Reactivities/client-app/src/app/stores/profileStore.ts
@@ -2,7 +2,7 @@ import { tr } from "date-fns/locale";
 import { action, observable, runInAction, computed, reaction } from "mobx";
 import { toast } from "react-toastify";
 import agent from "../api/agent";
-import { IPhoto, IProfile } from "../models/profile";
+import { IPhoto, IProfile, IUserActivity } from "../models/profile";
 import { RootStore } from "./rootStore";
 
 export default class ProfileStore {
@@ -27,6 +27,8 @@ export default class ProfileStore {
  @observable loading = false;
  @observable followings: IProfile[] = [];
  @observable activeTab: number = 0;
+ @observable userActivities: IUserActivity[] = [];
+ @observable loadingActivities = false;
 
  @computed get isCurrentUser() {
   if (this.rootStore.userStore.user && this.profile) {
@@ -34,6 +36,22 @@ export default class ProfileStore {
   } else {
    return false;
   }
+ }
+
+ @action loadUserActivities = async (username: string, predicate?: string) => {
+   this.loadingActivities = true;
+   try {
+    const activities = await agent.Profiles.listActivities(username, predicate!);
+    runInAction(() => {
+      this.userActivities = activities;
+      this.loadingActivities = false;
+    })
+   } catch (error) {
+     toast.error('Problem loading activities');
+     runInAction(() => {
+       this.loadingActivities = false;
+     })
+   }
  }
 
  @action loadProfile = async (username: string) => {
