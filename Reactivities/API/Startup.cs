69a2add9083b4bd87f20ffe0b7e54f1081ff5cc5
Miroslav <miroslav.miki.stojakovic@gmail.com>
2020-12-24 12:50:51 +0100

    Added checking of token exparation

--- Reactivities/API/Startup.cs
+++ Reactivities/API/Startup.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Text;
 using System.Threading.Tasks;
 using API.Middleware;
@@ -26,117 +27,123 @@ using Persistence;
 
 namespace API
 {
- public class Startup
- {
-  readonly string MyAllowSpecificOrigins = "EnableCORS";
-  public Startup(IConfiguration configuration)
+  public class Startup
   {
-   Configuration = configuration;
-  }
-
-  public IConfiguration Configuration { get; }
-
-  // This method gets called by the runtime. Use this method to add services to the container.
-  public void ConfigureServices(IServiceCollection services)
-  {
-   services.AddDbContext<DataContext>(opt =>
-   {
-    opt.UseLazyLoadingProxies();
-    opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
-   });
-   services.AddCors(opt =>
-   {
-    opt.AddPolicy(MyAllowSpecificOrigins, policy =>
+    readonly string MyAllowSpecificOrigins = "EnableCORS";
+    public Startup(IConfiguration configuration)
     {
-     policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000")
-      .AllowCredentials();
-    });
-   });
-   services.AddMediatR(typeof(List.Handler).Assembly);
-   services.AddAutoMapper(typeof(List.Handler));
-   services.AddSignalR();
-   services.AddControllers(opt =>
-   {
-    var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
-    opt.Filters.Add(new AuthorizeFilter(policy));
-   })
-     .AddFluentValidation(cfg =>
-     {
-      cfg.RegisterValidatorsFromAssemblyContaining<Create>();
-     });
+      Configuration = configuration;
+    }
 
-   var builder = services.AddIdentityCore<AppUser>();
-   var identityBuilder = new IdentityBuilder(builder.UserType, builder.Services);
-   identityBuilder.AddEntityFrameworkStores<DataContext>();
-   identityBuilder.AddSignInManager<SignInManager<AppUser>>();
+    public IConfiguration Configuration { get; }
 
-   services.AddAuthorization(opt =>
-   {
-    opt.AddPolicy("IsActivityHost", policy =>
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
     {
-     policy.Requirements.Add(new IsHostRequirement());
-    });
-   });
-
-   services.AddTransient<IAuthorizationHandler, IsHostRequirementHandler>();
-
-   var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["TokenKey"]));
-
-   services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(opt =>
-    {
-     opt.TokenValidationParameters = new TokenValidationParameters
-     {
-      ValidateIssuerSigningKey = true,
-      IssuerSigningKey = key,
-      ValidateAudience = false,
-      ValidateIssuer = false
-     };
-     opt.Events = new JwtBearerEvents
-     {
-      OnMessageReceived = context =>
+      services.AddDbContext<DataContext>(opt =>
       {
-       var accessToken = context.Request.Query["access_token"];
-       var path = context.HttpContext.Request.Path;
-       if (!string.IsNullOrEmpty(accessToken) && path.StartsWithSegments("/chat"))
+        opt.UseLazyLoadingProxies();
+        opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
+      });
+      services.AddCors(opt =>
+      {
+        opt.AddPolicy(MyAllowSpecificOrigins, policy =>
+     {
+       policy
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        .WithExposedHeaders("WWW-Authenticate")
+        .WithOrigins("http://localhost:3000")
+        .AllowCredentials();
+     });
+      });
+      services.AddMediatR(typeof(List.Handler).Assembly);
+      services.AddAutoMapper(typeof(List.Handler));
+      services.AddSignalR();
+      services.AddControllers(opt =>
+      {
+        var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+        opt.Filters.Add(new AuthorizeFilter(policy));
+      })
+        .AddFluentValidation(cfg =>
+        {
+          cfg.RegisterValidatorsFromAssemblyContaining<Create>();
+        });
+
+      var builder = services.AddIdentityCore<AppUser>();
+      var identityBuilder = new IdentityBuilder(builder.UserType, builder.Services);
+      identityBuilder.AddEntityFrameworkStores<DataContext>();
+      identityBuilder.AddSignInManager<SignInManager<AppUser>>();
+
+      services.AddAuthorization(opt =>
+      {
+        opt.AddPolicy("IsActivityHost", policy =>
+     {
+       policy.Requirements.Add(new IsHostRequirement());
+     });
+      });
+
+      services.AddTransient<IAuthorizationHandler, IsHostRequirementHandler>();
+
+      var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["TokenKey"]));
+
+      services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+       .AddJwtBearer(opt =>
        {
-        context.Token = accessToken;
-       }
+         opt.TokenValidationParameters = new TokenValidationParameters
+         {
+           ValidateIssuerSigningKey = true,
+           IssuerSigningKey = key,
+           ValidateAudience = false,
+           ValidateIssuer = false,
+           ValidateLifetime = true,
+           ClockSkew = TimeSpan.Zero
+         };
+         opt.Events = new JwtBearerEvents
+         {
+           OnMessageReceived = context =>
+        {
+          var accessToken = context.Request.Query["access_token"];
+          var path = context.HttpContext.Request.Path;
+          if (!string.IsNullOrEmpty(accessToken) && path.StartsWithSegments("/chat"))
+          {
+            context.Token = accessToken;
+          }
 
-       return Task.CompletedTask;
+          return Task.CompletedTask;
+        }
+         };
+       });
+
+      services.AddScoped<IJwtGenerator, JwtGenerator>();
+      services.AddScoped<IUserAccessor, UserAccessor>();
+      services.AddScoped<IPhotoAccessor, PhotoAccessor>();
+      services.AddScoped<IProfileReader, ProfileReader>();
+      services.Configure<CloudinarySettings>(Configuration.GetSection("Cloudinary"));
+
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      app.UseMiddleware<ErrorHandlingMiddleware>();
+      if (env.IsDevelopment())
+      {
+        //app.UseDeveloperExceptionPage();
       }
-     };
-    });
 
-   services.AddScoped<IJwtGenerator, JwtGenerator>();
-   services.AddScoped<IUserAccessor, UserAccessor>();
-   services.AddScoped<IPhotoAccessor, PhotoAccessor>();
-   services.AddScoped<IProfileReader, ProfileReader>();
-   services.Configure<CloudinarySettings>(Configuration.GetSection("Cloudinary"));
+      // app.UseHttpsRedirection();
 
+      app.UseRouting();
+      app.UseCors(MyAllowSpecificOrigins);
+
+      app.UseAuthentication();
+      app.UseAuthorization();
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapControllers();
+        endpoints.MapHub<ChatHub>("/chat");
+      });
+    }
   }
-
-  // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-  public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-  {
-   app.UseMiddleware<ErrorHandlingMiddleware>();
-   if (env.IsDevelopment())
-   {
-    //app.UseDeveloperExceptionPage();
-   }
-
-   // app.UseHttpsRedirection();
-
-   app.UseRouting();
-   app.UseCors(MyAllowSpecificOrigins);
-
-   app.UseAuthentication();
-   app.UseAuthorization();
-   app.UseEndpoints(endpoints =>
-   {
-    endpoints.MapControllers();
-    endpoints.MapHub<ChatHub>("/chat");
-   });
-  }
- }
 }
